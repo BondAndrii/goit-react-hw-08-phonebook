0cf4cb235d01ab5da0bca600506b7623412fa57a
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-06-11 13:16:02 +0300

    add controlled input for LoginForm and RegisterFor

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
 
 
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function reset() {
+        setEmail('');
+        setPassword(''); 
+    };
+    const handleChange = event => {
+        const { name, value } = event.currentTarget;
+
+        switch (name) {
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                return;
+        }
+        console.log("in change", value)
+    }
     return (
         <form>
             <label>
                 Emaill:
-                <input type="email" name="email"/>
+                <input type="email"
+                    name="email"
+                    value={email}
+                    placeholder="enter your email"
+                    onChange={handleChange}
+                />
             </label>
             <label>
                 Password:
-                <input type="password" name="password"/>
+                <input type="password"
+                    name="password"
+                    value={password}
+                    placeholder="remember your password"
+                    onChange={handleChange}
+                />
             </label>
             <button type="submit">Log in</button>
         </form>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,19 +1,69 @@
+import { useState } from "react";
 
 
 const RegisterForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function reset() {
+        setName('');
+        setEmail('');
+        setPassword(''); 
+    };
+
+    const handleChange = event => {
+        const { name, value } = event.currentTarget;
+
+        switch (name) {
+            case "userName":
+                setName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                return;
+        }
+        // console.log("in change", value)
+    }
+
+    
+
     return (
         <form>
             <label> 
                 Username: 
-                <input type="text" name="name"/>
+                <input
+                    type="text"
+                    name="userName"
+                    value={name}
+                    placeholder="enter your name"
+                    onChange={handleChange}
+                />
             </label>
             <label> 
                 Email: 
-                <input type="email" name="email"/>
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="enter your email"
+                    onChange={handleChange}
+                />
             </label>
             <label> 
                 Password: 
-                <input type="password" name="password"/>
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="create your password"
+                    onChange={handleChange}
+                />
             </label>
             <button type="submit">Register</button>
         </form>
